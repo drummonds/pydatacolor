4cfb3539ab04c40330b4f22903b3bacfe612c9bc
Humphrey Drummond <hum3@drummond.info>
2023-04-15 23:19:13 +0100

    First test code

---
+++ test.py
@@ -0,0 +1,20 @@
+import usb.core
+dev = usb.core.find()
+printers = usb.core.find(find_all=True, bDeviceClass=7)
+count = 0
+for printer in printers:
+    print(printer)
+    count += 1
+print(f'There are {count} printers in the system')
+devices = usb.core.find(find_all=True, bDeviceClass=7)
+count = 0
+for device in devices:
+    print(device)
+    count += 1
+print(f'There are {count} devices  in the system')
+dev = usb.core.find(idVendor=0x085c, idProduct=0x0007)
+if dev is None:
+    print ('Our device is not connected')
+
+
+print("Done this")
